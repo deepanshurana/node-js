98736a6c436b1d71d6124861d3b939c8223fc8d2
Deepanshu Rana <deepanshurana4500@gmail.com>
2023-05-30 10:48:27 +0530

    body parsers and added get and post products

--- express/middlewares.js
+++ express/middlewares.js
@@ -1,3 +1,6 @@
+const bodyParser = require("body-parser");
+module.exports.bodyParser = bodyParser.urlencoded({ extended: false });
+
 module.exports.thisAlwaysRunMiddleware = (path) => {
     return (req, res, next) => {
         console.log("this always run");
@@ -6,6 +9,43 @@ module.exports.thisAlwaysRunMiddleware = (path) => {
     };
 };
 
+module.exports.addProduct = (path) => {
+    return (req, res, next) => {
+        let html_body = "";
+        if (req.path === path) {
+            html_body = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <form action="/product" method="POST">
+                    <input type="text" name="title">
+                        <button type="submit"> Add product</button>
+                </form>
+            </body>
+            </html>`;
+
+            res.send(html_body);
+        } else {
+            next();
+        }
+    };
+};
+
+module.exports.allProducts = (path) => {
+    return (req, res, next) => {
+        if (req.path === path) {
+            console.log(req.body);
+            res.redirect("/");
+        } else {
+            next();
+        }
+    };
+};
 module.exports.checkFirstMiddleware = (path) => {
     return (req, res, next) => {
         if (req.path !== path) {
--- express/server.js
+++ express/server.js
@@ -1,11 +1,18 @@
 const {
+    bodyParser,
     thisAlwaysRunMiddleware,
     checkFirstMiddleware,
     checkNextMiddleware,
+    addProduct,
+    allProducts,
 } = require("./middlewares");
-const express = require("express");
 
+const express = require("express");
 const app = express();
+
+app.use(bodyParser);
+app.use(addProduct("/add-product"));
+app.use(allProducts("/product"));
 app.use(thisAlwaysRunMiddleware("/"));
 app.use(checkFirstMiddleware("/hello"));
 app.use(checkNextMiddleware);
