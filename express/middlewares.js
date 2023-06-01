77df55ccd53b393bbe7ae8586d3d194fbcf1c129
Deepanshu Rana <deepanshurana4500@gmail.com>
2023-06-01 19:15:07 +0530

    Removed console logs

--- express/middlewares.js
+++ express/middlewares.js
@@ -3,7 +3,6 @@ module.exports.bodyParser = bodyParser.urlencoded({ extended: false });
 
 module.exports.thisAlwaysRunsMiddleware = (path) => {
     return (req, res, next) => {
-        console.log(`Request URL: ${req.url} Method: ${req.method}`);
         next();
     };
 };
@@ -19,6 +18,5 @@ module.exports.checkFirstMiddleware = (path) => {
 };
 
 module.exports.checkNextMiddleware = (req, res, next) => {
-    console.log("Second Middleware.");
     next();
 };
